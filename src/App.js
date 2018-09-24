b17fddd1b0746adad46699846107aa6d430953bf
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-24 10:16:14 -0400

    Add pages for component development

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class BooksApp extends React.Component {
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
-     */
+     */ 
     showSearchPage: false
   }
 
