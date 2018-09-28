935dad448c02efc8d8b5c20e402698b48c77b2c5
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-28 12:12:45 -0400

    Import BooksAPI into Search component & set state for books searched for

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,6 @@
 import React, {Component } from 'react';
+import Book from './Book';
+import * as BooksAPI from './BooksAPI';
 
 class Search extends Component {
     state = {
@@ -21,14 +23,6 @@ class Search extends Component {
                 <div className="search-books-bar">
                     <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
                     <div className="search-books-input-wrapper">
-                    {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
                         <input type="text" 
                             placeholder="Search by title or author" 
                             value={this.state.query}
@@ -36,7 +30,16 @@ class Search extends Component {
                     </div>
                 </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                { this.state.searchedBooks.map(searchedBook => (
+                    <li key={searchedBook.id}>
+                        <Book 
+                            book={searchedBook}
+                        />
+                    </li>
+                ))
+                }
+              </ol>
             </div>
           </div>
         ); //closing bracket for return
