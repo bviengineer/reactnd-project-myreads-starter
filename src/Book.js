fe08917d861e4f608ad421fa6f50ff9848939e2d
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-28 14:57:30 -0400

    Assign thumbnail images to a variable and display one if present or an empty string otherwise

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,7 @@ import React, {Component } from 'react';
 
 class Book extends Component {
     render(){
+		const imgThumbnail = this.props.book.imageLinks.thumbnail ? this.props.book.imageLinks.thumbnail : "";
         return(
           <div className="book">
             <div className="book-top">
