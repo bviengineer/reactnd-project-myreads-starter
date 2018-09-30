6b286b2f28d966a86ad6a0c0dc0cd1f084a31e40
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-30 14:33:32 -0400

    Updated search code

--- src/Search.js
+++ src/Search.js
@@ -6,51 +6,60 @@ import * as BooksAPI from './BooksAPI';
 class Search extends Component {
     state = {
         query: "", 
-        searchedBooks: [] //array for books returned from search
+        searchBooks: [] //array for books returned from search
     }
 
     updateQuery = (query) => {
         this.setState({
             query: query
         })
-        // this.updateSearchedBooks(query)
+        this.updateSearch(query)
     }
 
-    getSearchedBooks = (query) => {
-      BooksAPI.search(query).then((searchedBooks) => {
-        this.setState({ searchedBooks: searchedBooks })
+   updateSearch = query => {
+		 if(query) {
+			BooksAPI.search(query).then(searchBooks => {
+				if(searchBooks.error) {
+					this.setState({ searchBooks: [] });
+				} else {
+					this.setState({ searchBooks: searchBooks });
+				}        
       })
-    }
+		 } else {
+			 this.setState({ searchBooks: []});
+		 }
+		};
+		
     render(){ 
         return( 
             <div className="search-books">
                 <div className="search-books-bar">
                     <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
                     <div className="search-books-input-wrapper">
-                        <input type="text" 
-                            placeholder="Search by title or author" 
-                            value={this.state.query}
-                            onChange={(e) => this.updateQuery(e.target.value)}/>
+											<input type="text" 
+												placeholder="Search by title or author" 
+												value={this.state.query}
+												onChange={(e) => this.updateQuery(e.target.value)}
+											/>
                     </div>
                 </div>
             <div className="search-books-results">
               <ol className="books-grid">
-                {/* {this.state.searchedBooks.map(searchedBook => { 
-                    let shelf = "none";
-
-                    this.props.books.map(book => (
-                        book.id === searchedBook.id ? shelf = book.shelf: "none"    
-                    ));
-
-                    return
-                    <li key={searchedBook.id}>
-                        <Book 
-                            individualBook={searchedBook}
-                            updateShelfForBook={this.props.updateShelf}
-                        />
-                    </li>
-                ))
-                } */}
+								{
+									this.state.searchBooks.map(searchBook => {
+										let shelf="none";
+										
+										this.props.library.map(book => (book.id === searchBook.id ? shelf = book.shelf : ""));
+										return (
+											<li key={searchBook.id}>
+												<Book 
+													book={searchBook}
+													changeShelf={this.props.changeShelf}
+													moveBooks={shelf}
+												/>
+											</li>
+										);
+									})}
               </ol>
             </div>
           </div>
