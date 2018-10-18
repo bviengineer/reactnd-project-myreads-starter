0b01c6f29b59fbe0e7c0e9d77d27abf23a710160
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-10-18 13:16:13 -0400

    Updated function within callback

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ class BooksApp extends React.Component {
   //Thank you to Amanda Hasenzahl (AmandaH) for her assistance on the efficieny in the changing of shelves  
   //Update shelves for each shelf type 
   updateShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then(this.stateUpdate())
+    BooksAPI.update(book, shelf).then((book) => {
+      this.stateUpdate();
+    });
 }
 
   render() {
