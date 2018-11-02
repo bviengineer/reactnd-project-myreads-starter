821f35d11433219f8f3f999f707cad6493a365e9
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-02 14:51:14 -0400

    Reafactor for easy readability

--- src/Book.js
+++ src/Book.js
@@ -6,33 +6,36 @@
     2. Learn ReactJS: Part1 on CodeCademy was beneficial in understanding how ReactJS works. 
 
   PURPOSE of Book.js:
-    Book.js handles the user shelf selection after the search results have been returned.  
+    Book.js handles changing of books between shelves  
 */
 
 import React from 'react';
 
-export const Book = (props) => {
+
+export const Book = (props) => {	
 	//if a book image does not exist, will display a blank space
 	let imgThumbnail = props.book.imageLinks ? props.book.imageLinks.thumbnail : "";
 
 	return(
 	<div className="book">
 		<div className="book-top">
-			<div style={{ boxShadow: `0px 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)`, background: `yellow`, width: 128, height: 193, backgroundImage: `url("${imgThumbnail}")` }}></div>
+			<div style={{ boxShadow: `0px 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)`, background: `yellow`, width: 128, height: 193, backgroundImage: `url("${imgThumbnail}")` }}>
+			</div>
 				<div className="book-shelf-changer">
-				<select onChange = { (e) => props.updateShelf(props.book, e.target.value)}
-					value={props.activeShelf} //highlights which shelf a book is on while on the search page
-				> {/* closing angle bracket for select element */}
-						<option value="move" disabled>Move to...</option>
-						<option value="currentlyReading">Currently Reading</option>
-						<option value="wantToRead">Want to Read</option>
-						<option value="read">Read</option>
-						<option value="none">None</option>
-					</select>
-				</div>
-				</div>
-			<div className="book-title">{props.book.title}</div>
-		<div className="book-authors">{props.book.authors}</div>
-	</div>
+							<select 
+								onChange = { (e) => props.updateShelf(props.book, e.target.value)} 
+								value={props.activeShelf} //highlights which shelf a book is on while on the search page
+							>
+								<option value="move" disabled>Move to...</option>
+								<option value="currentlyReading">Currently Reading</option>
+								<option value="wantToRead">Want to Read</option>
+								<option value="read">Read</option>
+								<option value="none">None</option>
+							</select>
+						</div>
+						</div>
+					<div className="book-title">{props.book.title}</div>
+				<div className="book-authors">{props.book.authors}</div>
+			</div>
 		);
 	}
