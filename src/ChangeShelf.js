3c0519a716d2f374b443b71f50412b153dc56568
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-05 13:43:17 -0500

    Change name of shelf

---
+++ src/ChangeShelf.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+//Rendering in Book.js
+export class ChangeShelf extends React.Component{
+  render(){
+    return(
+      <div className="book-shelf-changer">
+        <select 
+          onChange = { (e) => this.props.updateShelf(this.props.book, e.target.value)} 
+          value={this.props.activeShelf} >
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
