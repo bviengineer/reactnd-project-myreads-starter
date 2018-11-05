11cc464d053b5075e2bfe80915fe339ec99c8d39
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-05 12:45:53 -0500

    imported shelfChanger

--- src/Book.js
+++ src/Book.js
@@ -10,33 +10,42 @@
 */
 
 import React from 'react';
+import { ShelfChanger } from "./shelfChanger.js"
 
 
 //Rendering in Search.js, reads.js, desiredReeds.js, currentReads.js
-export const Book = (props) => {	
-	//if a book image does not exist, will display a blank space
-	let imgThumbnail = props.book.imageLinks ? props.book.imageLinks.thumbnail : "";
+export class Book extends React.Component {	
+	// componentDidMount(){
+	// 	console.log("props on book.js component ", this)
+	// }
 
-	return(
-	<div className="book">
-		<div className="book-top">
-			<div style={{ boxShadow: `0px 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)`, background: `yellow`, width: 128, height: 193, backgroundImage: `url("${imgThumbnail}")` }}>
-			</div>
-				<div className="book-shelf-changer">
-							<select 
-								onChange = { (e) => props.updateShelf(props.book, e.target.value)} 
-								value={props.activeShelf} //highlights which shelf a book is on while on the search page
-							>
-								<option value="move" disabled>Move to...</option>
-								<option value="currentlyReading">Currently Reading</option>
-								<option value="wantToRead">Want to Read</option>
-								<option value="read">Read</option>
-								<option value="none">None</option>
-							</select>
-						</div>
-						</div>
-					<div className="book-title">{props.book.title}</div>
-				<div className="book-authors">{props.book.authors}</div>
-			</div>
-		);
-	}
+	render(){
+		//if a book image does not exist, will display a blank space
+		let imgThumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : "";
+
+		return(
+		<div className="book">
+			<div className="book-top">
+				<div style={{ boxShadow: `0px 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)`, background: `yello
+				w`, width: 128, height: 193, backgroundImage: `url("${imgThumbnail}")` }}>
+				</div>
+					<ShelfChanger book={this.props.book} updateShelf={this.props.updateShelf} activeShelf={this.props.activeShelf}/>
+					{/* <div className="book-shelf-changer">
+								<select 
+									onChange = { (e) => this.props.updateShelf(this.props.book, e.target.value)} 
+									value={this.props.activeShelf} //highlights which shelf a book is on while on the search page
+								>
+									<option value="move" disabled>Move to...</option>
+									<option value="currentlyReading">Currently Reading</option>
+									<option value="wantToRead">Want to Read</option>
+									<option value="read">Read</option>
+									<option value="none">None</option>
+								</select>
+							</div> */}
+							</div>
+						<div className="book-title">{this.props.book.title}</div>
+					<div className="book-authors">{this.props.book.authors}</div>
+				</div>
+			);
+		}
+}
