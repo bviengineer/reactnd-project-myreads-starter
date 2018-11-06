f1650fdf548cb905b4668be6233b862465fc65d3
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-11-06 00:45:13 -0500

    Update comment(s) and refactor as a statelesss functional component

--- src/ChangeShelf.js
+++ src/ChangeShelf.js
@@ -1,26 +1,19 @@
 import React from "react";
 
 
-//Rendering in Book.js
-export class ChangeShelf extends React.Component{
-  render(){
-    return(
-      <div className="book-shelf-changer">
-        <select 
-          onChange = { (e) => this.props.updateShelf(this.props.book, e.target.value)} 
-          value={this.props.activeShelf} >
-          <option value="move" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
-  }
+//Rendering in Book.js - will handle the changing of books between shelves
+export const ChangeShelf = (props) => {
+  return(
+    <div className="book-shelf-changer">
+      <select 
+        onChange = { (e) => props.updateShelf(props.book, e.target.value)} 
+        value={props.activeShelf} >
+        <option value="move" disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
 }
-
-/*
-Will like to know if having the props being inherited here also needs to be inherited in the component instance where the component ChangeShelf is being rendered
-
-*/
